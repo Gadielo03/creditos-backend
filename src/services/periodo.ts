15282bacaaf7dea152511ecf715cda77fa0e4d7f
pacho ruiz <alfrrugu@hotmail.com>
2025-11-13 19:28:22 -0600

    feat(usuarios): periodo n shit

--- src/services/periodo.ts
+++ src/services/periodo.ts
@@ -29,7 +29,6 @@ const getAllPeriodos = async (): Promise<Periodo[]> => {
 
 const createPeriodo = async (periodo: Partial<Periodo>): Promise<Periodo> => {
     const client = await pool.connect();
-
     if (!periodo.inicio || !periodo.fin || !periodo.nombre) {
         throw new Error('Missing required fields to create Periodo');
     }
@@ -54,6 +53,60 @@ const createPeriodo = async (periodo: Partial<Periodo>): Promise<Periodo> => {
     } catch (error) {
         console.error('Database query error: ', error);
         throw error;
+    } finally {
+        client.release();
+    }
+}
+
+const updatePeriodo = async (periodo: Periodo): Promise<Periodo> => {
+    const periodoExits = (await getAllPeriodos()).filter(p => p.id === periodo.id);
+    if (periodoExits.length === 0) {
+        throw new Error(`Periodo with id ${periodo.id} does not exist`);
+    }
+
+    const client = await pool.connect();
+    try {
+        const query = `
+        UPDATE periodo
+        SET per_inicio = $1, per_fin = $2, per_nombre = $3
+        WHERE per_id = $4
+        RETURNING per_id, per_inicio, per_fin, per_nombre
+        `;
+        const values = [periodo.inicio, periodo.fin, periodo.nombre, periodo.id];
+        const res = await client.query(query, values);
+        const row: PeriodoDB = res.rows[0];
+
+        return {
+            id: row.per_id,
+            inicio: row.per_inicio,
+            fin: row.per_fin,
+            nombre: row.per_nombre
+        };
+    } catch (error) {
+        console.error('Database query error: ', error);
+        throw error;
+    } finally {
+        client.release();
+    }
+}
+
+const deletePeriodo = async (id: number): Promise<boolean> => {
+    const periodoExits = (await getAllPeriodos()).filter(p => p.id === id);
+    if (periodoExits.length === 0) {
+        throw new Error(`Periodo with id ${id} does not exist`);
+    }
+
+    const client = await pool.connect();
+    try {
+        const query = `DELETE FROM periodo WHERE per_id = $1`;
+        const values = [id];
+        await client.query(query, values);
+        return true;
+    } catch (error) {
+        console.error('Database query error: ', error);
+        throw error;
+    } finally {
+        client.release();
     }
 }
 
@@ -62,7 +115,9 @@ const dataRetreive = {
 }
 
 const dataModify = {
-    createPeriodo
+    createPeriodo,
+    updatePeriodo,
+    deletePeriodo
 }
 
 const servicioPeriodo = {
